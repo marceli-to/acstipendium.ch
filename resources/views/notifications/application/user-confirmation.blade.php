9805c0564b5988134ac5f507e9e6139a88719664
Marcel Stadelmann <m@marceli.to>
2025-11-03 13:53:45 +0100

    adds logo to mail

--- resources/views/notifications/application/user-confirmation.blade.php
+++ resources/views/notifications/application/user-confirmation.blade.php
@@ -8,6 +8,14 @@ Wir danken Ihnen für Ihr Interesse am AC-Stipendium und wünschen Ihnen bis dah
 Nous vous remercions pour la soumission de votre dossier de candidature à la bourse Aeschlimann Corti. Votre candidature nous est bien parvenue et sera examinée dans le cadre du processus de sélection.<br><br>
 Si des questions devaient survenir lors de l'examen de vos documents, nous prendrons contact avec vous. À l'issue du premier tour de jurys, nous vous recontacterons à nouveau.<br><br>
 Nous vous remercions de l'intérêt que vous portez à la bourse AC et vous souhaitons, d'ici là, nos meilleurs vœux de réussite.<br><br>
-Freundliche Grüsse / Cordialement
+Freundliche Grüsse / Cordialement<br><br>
+<div style="padding-bottom: 8px">
+<img src="{{ asset('img/logo.png') }}" alt="Louise Aeschlimann und Margareta Corti Stiftung" style="width: 350px; height: auto; display: block;">
+</div>
+Louise Aeschlimann und Margareta Corti Stiftung<br>
+Hodlerstrasse 8–12<br>
+CH–3011 Bern<br>
+info@ac-stipendium.ch<br>
+www.ac-stipendium.ch
 </div>
 </x-mail::message>
